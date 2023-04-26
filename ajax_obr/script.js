a2e2d9f3afe482a377337f4a817acdb49fddb4f9
pmcu <pmcu@users.noreply.github.com>
2023-04-26 16:03:51 +0100

    Update script.js

--- ajax_obr/script.js
+++ ajax_obr/script.js
@@ -1,13 +1,22 @@
-const input = document.getElementById('input');
-const output = document.getElementById('output');
-const writeBtn = document.getElementById('writeBtn');
-const readBtn = document.getElementById('readBtn');
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+        myFunction(xhttp);
+    }
+};
+xhttp.open("GET", "othuaidh_simp.xml", true);
+xhttp.send();
 
-writeBtn.addEventListener('click', () => {
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+  
 
-    let val = input.value;
-    output.innerHTML = val;
-
-});
-myFunction(xml);
-
+    var x;
+    const input = document.getElementById('input');
+    i = input;
+    x = xmlDoc.getElementsByTagName("leath")[i].childNodes[0];
+    document.getElementById("demo1").innerHTML = x.nodeValue;
+i=i+1;
+    x = xmlDoc.getElementsByTagName("leath")[i].childNodes[0];
+    document.getElementById("demo2").innerHTML = x.nodeValue;
+}
